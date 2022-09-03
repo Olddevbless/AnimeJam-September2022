bf2965969a8eb7e59363e28efc86a574e361cada
Olddevbless <Olddevbless@users.noreply.github.com>
2022-09-03 10:05:57 +0300

    initial commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    
+    [SerializeField] int speed = 1;
+    [SerializeField] Vector2 mousePos;
+    [SerializeField] GameObject kickSprite;
+    [SerializeField] GameObject punchSprite;
+    public int playerHealth;
+    [SerializeField] int playerMaxHealth;
+    float heavyAttackCD = 2f;
+    float heavyAttackCount;
+    float lightAttackCD = 0.5f;
+    float lightAttackCount;
+    public Camera cam;
+    Rigidbody2D rb;
+    Transform aim;
+    Vector2 movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.constraints = RigidbodyConstraints2D.None;
+        playerHealth = playerMaxHealth;
+        rb = this.GetComponent<Rigidbody2D>();
+        aim = gameObject.GetComponentInChildren<Transform>();
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+    }
+    private void FixedUpdate()
+    {
+        Movement();
+        MouseAim();
+        Attack();
+    }
+
+    void Movement()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+
+    }
+
+    void MouseAim()
+    {
+       
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg -90f ;
+        rb.rotation = angle;
+        
+    }
+    void Attack()
+    {
+        
+        
+        if (Input.GetAxisRaw("Fire2")>0 && heavyAttackCount<=0f)
+        {
+            kickSprite.SetActive(true);
+            heavyAttackCount = heavyAttackCD;
+        }
+        else
+        {
+            kickSprite.SetActive(false);
+        }
+        if (heavyAttackCount>0 )
+        {
+            
+            heavyAttackCount -= Time.deltaTime;
+        }
+        if (Input.GetAxisRaw("Fire1") > 0 && lightAttackCount <= 0f)
+        {
+            punchSprite.SetActive(true);
+            lightAttackCount = lightAttackCD;
+        }
+        else
+        {
+            punchSprite.SetActive(false);
+        }
+        if (lightAttackCount>0)
+        {
+            lightAttackCount -= Time.deltaTime;
+        }
+    }
+    void TakeDamage(int damage)
+    {
+        playerHealth -= damage;
+    }
+    void Death()
+    {
+        if (playerHealth <=0)
+        {
+            rb.constraints = RigidbodyConstraints2D.FreezeAll;
+        }
+    }
+}
