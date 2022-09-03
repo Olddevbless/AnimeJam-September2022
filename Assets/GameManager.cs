5fd4cda2a8b5bcf32978c1f5e992261e4a359415
Olddevbless <Olddevbless@users.noreply.github.com>
2022-09-03 19:23:12 +0300

    pause menu

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,18 +1,66 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public static GameManager current;
+    public GameObject pauseMenu;
+    public bool pauseMenuIsActive;
+    public GameManager instance;
+    public int score;
+    
+    void Awake()
     {
-        
+        current = this;
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(this);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseMenu();
+        }
+    }
+
+    
+    void PauseMenu()
+    {
+        pauseMenuIsActive = !pauseMenuIsActive;
+        if (pauseMenuIsActive)
+        {
+            Debug.Log("pausing");
+            Debug.Log(Time.timeScale);
+            Time.timeScale = 0;
+            pauseMenu.SetActive(true);
+        }
+        else
+        {
+            Time.timeScale = 1;
+            pauseMenu.SetActive(false);
+        }
+
+    }
+    void ExitApplication()
+    {
+        Application.Quit();
+    }
+    void Resume()
+    {
+        pauseMenuIsActive = false;
+        pauseMenu.SetActive(false);
     }
 }
